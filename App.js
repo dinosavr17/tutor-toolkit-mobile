861bafae32f5f2a140adf531ff1572918ea75f18
dinosavr_17 <studypost1701@gmail.com>
2024-01-29 11:25:10 +0500

    Adding register component

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Register from './components/Authentification/Register';
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Артем чуча</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Register">
+          <Drawer.Screen name="Регистрация" component={Register} />
+        </Drawer.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
